e7e757ca0083fc8673944c28d2913653f93d5b61
aa <aa>
2019-08-26 22:53:57 +0800

    Testlocal

--- discovery-eureka/src/main/java/com/study/discoveryeureka/DiscoveryEurekaApplication.java
+++ discovery-eureka/src/main/java/com/study/discoveryeureka/DiscoveryEurekaApplication.java
@@ -3,9 +3,11 @@ package com.study.discoveryeureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @EnableEurekaServer
 @SpringBootApplication
+@EnableHystrixDashboard
 public class DiscoveryEurekaApplication {
 
 	public static void main(String[] args) {
